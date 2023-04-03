42216052cb0eaa246cfb7fb1a2f2a15181b98749
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2023-04-03 23:27:22 +0200

    * libc/crt0.s: Some optimizations to the code.

--- libc/crt0.s
+++ libc/crt0.s
@@ -8,40 +8,25 @@
 _start:
 	mov r1, #0
 	mov lr, r1
+	mov r4, r9
+	push {r0, r4, r7, lr}
 	ldr r0, .Lbss_start_got
-	add r0, r0, r9
-	ldr r0, [r0]
+	ldr r0, [r0, r4]
 	ldr r2, .Lbss_end_got
-	add r2, r2, r9
-	ldr r2, [r2]
-	ldr r7, .Lmemset_got
-	add r7, r7, r9
-	bl  .Lbxpoline
+	ldr r2, [r2, r4]
+	bl  memset
 	mov r0, #0
 	mov r1, #0
 	mov r2, #0
-	ldr r7, .Lmain_got
-	add r7, r7, r9
-	bl  .Lbxpoline
+	bl  main
 	mov r7, #0
 	mov r0, #0
 	swi 28     @ exit(0)
 
-	.align 4
-.Lmemset_got:
-	.word 0x0
-	.reloc .Lmemset_got, R_ARM_GOT32, memset
-.Lmain_got:
-	.word 0x0
-	.reloc .Lmain_got, R_ARM_GOT32, main
+	.align 2
 .Lbss_start_got:
-	.word 0x0
-	.reloc .Lbss_start_got, R_ARM_GOT32, __bss_start__	
+	.word __bss_start__(GOT)
 .Lbss_end_got:
-	.word 0x0
-	.reloc .Lbss_end_got, R_ARM_GOT32, __bss_end__
-
-.Lbxpoline:
-	bx r7
+	.word __bss_end__(GOT)
 
 	.endfunc
